b16f9aaaf3c41e3f35cc50afa962d0db5e78d8e4
Aykutak04 <aykutak.04.55@gmail.com>
2025-11-18 09:08:43 +0100

    push

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import "dotenv/config"; // <- bu satır çok önemli, .env dosyasını otomatik 
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "lib/generated/prisma/schema.prisma",
+  schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
